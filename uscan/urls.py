b978ddb65c4efc4abf9ecc1a14a3f171f254cd11
root <root@ubuntu-ustudy.localdomain>
2017-03-26 08:34:56 +0000

    changed urls.py to remove unnecessary urls

--- uscan/urls.py
+++ uscan/urls.py
@@ -21,10 +21,8 @@ from goodie import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
-#   url(r'^request/', views.event_request, name='eventrequest'),
     url(r'^check/(?P<event_code>\w+)/', views.check_event),
     url(r'^manual/(?P<event>\w+)/',views.manual_register, name='manualregister'),
-    # url(r'^register/(?P<event>\w+)/(?P<matric_number>\w+)', views.register, name='register'),
     url(r'^register/', csrf_exempt(views.register), name='register'),
     url(r'^deleteMatric/', csrf_exempt(views.deleteMatric), name='deleteMatric'),
     url(r'^list/(?P<event>\w+)/', views.list_matric),
